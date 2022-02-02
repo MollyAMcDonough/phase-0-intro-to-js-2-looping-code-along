bf61853ee908a693baf0a2d34ab595d74bb29272
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-02-02 13:58:17 -0500

    commit tree c86ce1a5931691b2ec02af347238926142549912

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(nameArray,event) {
+    const messages = [];
+    for (let i=0; i<nameArray.length; i++) {
+        messages[i] =  `Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`;
+        debugger;
+    }
+    console.log(messages);
+    return messages;
+}
+
+function countDown(integar) {
+    let i = integar
+    while (i>=0) {
+        console.log(i)
+        i--;
+    }
+}
